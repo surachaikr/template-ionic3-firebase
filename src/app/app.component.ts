4efaba0b7570eaffb17a2d292478cf84f7cf0bd1
surachai kriwatcharin <surachai kriwatcharin>
2017-05-28 12:28:23 +0700

    - config npm, firebase, ngx translate, algolia, geolocation, android and browser platform

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import firebase from "firebase";
+import { Http } from '@angular/http';
 
 import { HomePage } from '../pages/home/home';
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  rootPage: any = HomePage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
@@ -16,6 +18,17 @@ export class MyApp {
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+    });
+  }
+
+  initApp() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyBW3wgFwhHIUHFJtHbaM43-kYA5VPnF1iA",
+      authDomain: "templateionic3firebase.firebaseapp.com",
+      databaseURL: "https://templateionic3firebase.firebaseio.com",
+      projectId: "templateionic3firebase",
+      storageBucket: "templateionic3firebase.appspot.com",
+      messagingSenderId: "262518369997"
     });
   }
 }
